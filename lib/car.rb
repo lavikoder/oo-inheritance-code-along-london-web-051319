4a73a4fcc3b37011759661d3545bd009e5aa032e
lavikoder <kevin.kamanda@flatironschool.com>
2019-05-21 17:54:23 +0100

    done

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+#require_relative allows us to have access to the methods in Vehicle class
+
+
+class Car < Vehicle
+  #inheriting from vehicle class
+  def go
+    #however it is possible to overwirte exisiting methods in the class
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
